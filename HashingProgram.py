8ccfd6df9adcdaa511e5910c9561b6308d1d07fa
Daniel Alfaro R <daniel.alfaro@upr.edu>
2019-12-06 14:15:16 -0400

    Output files now contain passwords of same length

--- HashingProgram.py
+++ HashingProgram.py
@@ -4,19 +4,16 @@ import random
 import string
 
 def createPasswordEasy(length):
-    letters = string.ascii_letters
+    letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
 def createPasswordMedium(length):
-    lettersAndDigits = string.ascii_letters + string.digits
+    lettersAndDigits = string.ascii_lowercase + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(length))
 
 def createPasswordHard(length):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(password_characters) for i in range(length))
-
-# def hashPassword(password):
-#      return hashlib.sha256(password.encode()).hexdigest()
 
 def md5(length, f):
     print("Easy Passwords: ")
@@ -71,33 +68,6 @@ def sha1(length, f):
         f.write(hash)
         f.write("\n")
     
-
-def sha256(length, f):
-    print("Easy Passwords: ")
-    for i in range(10):
-        password = createPasswordEasy(length)
-        hash = hashlib.sha256(password.encode()).hexdigest()
-        print("Password: ", password)
-        print("Hash: ", hash)
-        f.write(hash)
-        f.write("\n")
-    print("Medium Passwords: ")
-    for i in range(10):
-        password = createPasswordMedium(length)
-        hash = hashlib.sha256(password.encode()).hexdigest()
-        print("Password: ", password)
-        print("Hash: ", hash)
-        f.write(hash)
-        f.write("\n")
-    print("Hard Passwords: ")
-    for i in range(10):
-        password = createPasswordHard(length)
-        hash = hashlib.sha256(password.encode()).hexdigest()
-        print("Password: ", password)
-        print("Hash: ", hash)
-        f.write(hash)
-        f.write("\n")
-
 def sha512(length, f):
     print("Easy Passwords: ")
     for i in range(10):
@@ -125,32 +95,40 @@ def sha512(length, f):
         f.write("\n")
 
 def main():
-    f = open("MD5hashes.txt","w+")
-    md5(8, f)
-    md5(10, f)
-    md5(12, f)
-    md5(14, f)
+    f = open("MD5hashes6.txt","w+")
+    md5(6, f)
     f.close()
 
-    f2 = open("SHA1hashes.txt","w+")
-    sha1(8, f2)
-    sha1(10, f2)
-    sha1(12, f2)
-    sha1(14, f2)
+    f1 = open("MD5hashes7.txt","w+")
+    md5(7, f1)
+    f1.close()
+
+    f2 = open("MD5hashes8.txt","w+")
+    md5(8, f2)
     f2.close()
 
-    f3 = open("SHA256hashes.txt","w+")
-    sha256(8, f3)
-    sha256(10, f3)
-    sha256(12, f3)
-    sha256(14, f3)
+    f3 = open("SHA1hashes6.txt","w+")
+    sha1(6, f3)
     f3.close()
-
-    f4 = open("SHA512hashes.txt","w+")
-    sha512(8, f4)
-    sha512(10, f4)
-    sha512(12, f4)
-    sha512(14, f4)
+    
+    f4 = open("SHA1hashes7.txt","w+")
+    sha1(7, f4)
     f4.close()
+
+    f5 = open("SHA1hashes8.txt","w+")
+    sha1(8, f5)
+    f5.close()
+
+    f6 = open("SHA512hashes6.txt","w+")
+    sha512(6, f6)
+    f6.close()
+
+    f7 = open("SHA512hashes7.txt","w+")
+    sha512(7, f7)
+    f7.close()
+
+    f8 = open("SHA512hashes8.txt","w+")
+    sha512(8, f8)
+    f8.close()
     
 main()
